74c3160337d671d1e0e3cb62762dfc5d6f840df8
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-21 16:12:44 +0300

    feat: set working with socket.io

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let app = require('express')();
 let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected')
+    })
 });
 
 http.listen(3000, () => {
