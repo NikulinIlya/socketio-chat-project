e9152c9436a2b914f9e633e5fdb1470f1451b56d
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-21 19:07:13 +0300

    feat: add info string that a user joined/leaved chat

--- index.js
+++ index.js
@@ -19,18 +19,23 @@ io.on('connection', (socket) => {
 
     socket.on('chat-message', (data) => {
         socket.broadcast.emit('chat-message', (data))
-
-    })
+    });
 
     socket.on('typing', (data) => {
         socket.broadcast.emit('typing', (data))
-
-    })
+    });
 
     socket.on('stopTyping', (data) => {
         socket.broadcast.emit('stopTyping', (data))
+    });
 
-    })
+    socket.on('joined', (data) => {
+        socket.broadcast.emit('joined', (data))
+    });
+
+    socket.on('leaved', (data) => {
+        socket.broadcast.emit('leaved', (data))
+    });
 });
 
 http.listen(3000, () => {
